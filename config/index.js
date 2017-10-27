f81bf2f6fbf39aef7096eb199d2efec67119cfee
tianyu <344763278@qq.com>
2017-10-27 16:53:23 +0800

    修改打包目录为dist

--- config/index.js
+++ config/index.js
@@ -4,10 +4,10 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../static/index.html'),
-    assetsRoot: path.resolve(__dirname, '../static'),
-    assetsSubDirectory: 'other',
-    assetsPublicPath: '/static/',
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular other hosts such as
     // Surge or Netlify already gzip all other assets for you.
